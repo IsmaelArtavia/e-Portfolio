cf94e5c506e0942ac3b3ed0223296643f42d3b2f
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-03-24 17:54:03 +0000

    actualizacion 2

--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -1,42 +1,45 @@
 import React from "react";
 import "../../styles/Contact.scss";
 import { Link } from "react-router-dom";
+import Particles from "react-tsparticles";
 
 const Contact = () => {
 	return (
-		<div className="card">
-			<div className="card-header">Junior Full Stack Development</div>
-			<div className="card-body">
-				<h5 className="card-title">Email: ismaelartavia@yahoo.es</h5>
-				<p className="card-text">Phone number: 21010619</p>
-				<p className="card-text">Social media:</p>
+		<div className="container">
+			<div className=" card position-absolute top-100 start-200 translate-middle">
+				<div className="card-header">Junior Full Stack Development</div>
+				<div className="card-body">
+					<h5 className="card-title">Email: ismaelartavia@yahoo.es</h5>
+					<p className="card-text">Phone number: 21010619</p>
+					<p className="card-text">Social media:</p>
 
-				<ul>
-					<li>
-						<a href="https://github.com/IsmaelArtavia" target="_blank" rel="noopener noreferrer">
-							<span />
-							<span />
-							<span />
-							<span />
-							<span className="fa fa-github" />
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://www.linkedin.com/in/ismael-artavia-1746b0185/"
-							target="_blank"
-							rel="noopener noreferrer">
-							<span />
-							<span />
-							<span />
-							<span />
-							<span className="fa fa-linkedin" />
-						</a>
-					</li>
-				</ul>
-				<Link to="/">
-					<button className="btn btn-primary">Return</button>
-				</Link>
+					<ul>
+						<li>
+							<a href="https://github.com/IsmaelArtavia" target="_blank" rel="noopener noreferrer">
+								<span />
+								<span />
+								<span />
+								<span />
+								<span className="fa fa-github" />
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.linkedin.com/in/ismael-artavia-1746b0185/"
+								target="_blank"
+								rel="noopener noreferrer">
+								<span />
+								<span />
+								<span />
+								<span />
+								<span className="fa fa-linkedin" />
+							</a>
+						</li>
+					</ul>
+					<Link to="/">
+						<button className="btn btn-primary">Return</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
