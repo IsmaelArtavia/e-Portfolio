8cf549396e77a1da7819f3cea5cd9b8c298fdc43
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-03-25 02:34:47 +0000

    5 actualizacion

--- src/js/views/SWOT.js
+++ src/js/views/SWOT.js
@@ -6,7 +6,7 @@ const SWOT = () => {
 		<div className="container">
 			<section className="section-plans" id="section-plans">
 				<div className="u-center-text u-margin-bottom-big">
-					<h2 className="heading-secondary">Hosting Plans</h2>
+					<h2 className="heading-secondary">strengths and Weaknesses</h2>
 				</div>
 
 				<div className="row">
@@ -54,30 +54,37 @@ const SWOT = () => {
 						<div className="card">
 							<div className="card__side card__side--front-2">
 								<div className="card__title card__title--2">
-									<i className="fas fa-plane" />
-									<h4 className="card__heading">Plus</h4>
+									<i className="fas fa-unlink" />
+									<h4 className="card__heading"> WEAKNESSES</h4>
 								</div>
 
 								<div className="card__details">
-									<ul>
-										<li>Includes Basic Package Features</li>
-										<li>Unlimited Websites</li>
-										<li>Unlimited SSD Storage</li>
-										<li>Unlimited Domains</li>
-										<li>Unlimited Parked Domains</li>
-										<li>Unlimited Sub Domains</li>
-									</ul>
+									<h3>
+										What could you improve? <br />
+										Where do you have fewer resources than others?
+										<br />
+										What are others likely to see as weaknesses?
+									</h3>
 								</div>
 							</div>
 							<div className="card__side card__side--back card__side--back-2">
 								<div className="card__cta">
 									<div className="card__price-box">
-										<p className="card__price-only">Only</p>
-										<p className="card__price-value">$5.45/mo*</p>
+										<i className="fas fa-unlink" />
+										<h4 className="card__heading">WEAKNESSES</h4>
+										<p className="card__price-only">
+											<h3>
+												Perseverance <br />
+												Curiosity and willingness to learn <br />
+												Teamwork <br />
+												Analytical thinking <br />
+												Honesty <br />
+												Commitment <br />
+												Responsibility <br />
+												Discipline <br />
+											</h3>
+										</p>
 									</div>
-									<a href="#popup" className="btn btn--white">
-										Select
-									</a>
 								</div>
 							</div>
 						</div>
@@ -87,7 +94,7 @@ const SWOT = () => {
 
 			<section className="section-plans" id="section-plans">
 				<div className="u-center-text u-margin-bottom-big">
-					<h2 className="heading-secondary">Hosting Plans</h2>
+					<h2 className="heading-secondary">Opportunities And Threats</h2>
 				</div>
 
 				<div className="row">
@@ -96,29 +103,39 @@ const SWOT = () => {
 							<div className="card__side card__side--front-1">
 								<div className="card__title card__title--1">
 									<i className="fas fa-paper-plane" />
-									<h4 className="card__heading">Basic</h4>
+									<h4 className="card__heading">Opportunities</h4>
 								</div>
 
 								<div className="card__details">
-									<ul>
-										<li>1 Website</li>
-										<li>50 GB SSD Storage</li>
-										<li>Unmetered Bandwidth</li>
-										<li>Free SSL Certificate</li>
-										<li>1 Included Domain</li>
-										<li>1 Included Domain</li>
-									</ul>
+									<h3>
+										What opportunities are open to you? <br />
+										What trends could you take advantage of? <br />
+										How can you turn your strengths into opportunities?
+									</h3>
 								</div>
 							</div>
 							<div className="card__side card__side--back card__side--back-1">
 								<div className="card__cta">
 									<div className="card__price-box">
-										<p className="card__price-only">Only</p>
-										<p className="card__price-value">$2.95/mo*</p>
+										<i className="fas fa-paper-plane" />
+										<h4 className="card__heading">Opportunities</h4>
+										<p className="card__price-only">
+											<h3>
+												Second language <br />
+												Learn about technology
+												<br />
+												Government scholarships
+												<br />
+												Change the work paradigm because Covid 19 <br />
+												Reinvent itself
+												<br />
+												Take courses in other areas
+												<br />
+												Learn new things (cook) <br />
+												Adapt itself <br />
+											</h3>
+										</p>
 									</div>
-									<a href="#popup" className="btn btn--white">
-										Select
-									</a>
 								</div>
 							</div>
 						</div>
@@ -128,30 +145,37 @@ const SWOT = () => {
 						<div className="card">
 							<div className="card__side card__side--front-2">
 								<div className="card__title card__title--2">
-									<i className="fas fa-plane" />
-									<h4 className="card__heading">Plus</h4>
+									<i className="fas fa-bolt" />
+									<h4 className="card__heading">Threats</h4>
 								</div>
 
 								<div className="card__details">
-									<ul>
-										<li>Includes Basic Package Features</li>
-										<li>Unlimited Websites</li>
-										<li>Unlimited SSD Storage</li>
-										<li>Unlimited Domains</li>
-										<li>Unlimited Parked Domains</li>
-										<li>Unlimited Sub Domains</li>
-									</ul>
+									<h3>
+										What threats do you think could harm you in any way? <br />
+										What threats do you think your weaknesses expose to you?
+									</h3>
 								</div>
 							</div>
 							<div className="card__side card__side--back card__side--back-2">
 								<div className="card__cta">
 									<div className="card__price-box">
-										<p className="card__price-only">Only</p>
-										<p className="card__price-value">$5.45/mo*</p>
+										<i className="fas fa-bolt" />
+										<h4 className="card__heading">Threats</h4>
+										<p className="card__price-value">
+											<h3>
+												Third language <br />
+												Certifications <br />
+												People with more knowledge <br />
+												Age
+												<br />
+												More languages
+												<br />
+												Soft skills <br />
+												Knowledge update <br />
+												Sell me as a programmer <br />
+											</h3>
+										</p>
 									</div>
-									<a href="#popup" className="btn btn--white">
-										Select
-									</a>
 								</div>
 							</div>
 						</div>
